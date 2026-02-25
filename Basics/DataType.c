896a68dd9cf7c3e1e3e72a4c527da7624593fb6c
Raman verma <raman02verma@gmail.com>
2026-02-25 14:07:10 +0530

    Add c data type

---
+++ Basics/DataType.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    a = 25;
+    b = 15.5;
+    
+     printf("a+b= %.2f\n",a+b );
+     printf("a-b= %5.2f\n",a-b );
+     printf("a/b= %.2f\n",a/b );
+     printf("a*b= %10.2f\n",a*b );
+
+    return 0;
+} 
