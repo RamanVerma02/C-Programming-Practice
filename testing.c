2f4bcec07990094b9bb7d4bd8c411a343e77a868
Raman verma <raman02verma@gmail.com>
2026-02-25 14:51:24 +0530

    added rest program

---
+++ testing.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+int i , j;
+printf("Enter the number you want multiplication of\n");
+scanf("%d",&i);
+
+for(j=1 ; j<=10 ; j++)
+{ 
+    printf("%d\n", i*j);
+}
+return 0;
+}
+
+
