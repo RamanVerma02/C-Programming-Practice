c632f04773a547e1f6a7fdb8736453c64b895f31
Raman verma <raman02verma@gmail.com>
2026-02-25 14:44:40 +0530

    Add vowel or consonent by switch case

---
+++ Switch/VowelORConsonentBySwitch.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+     // C program to check vowel or consonant using switch case
+    char ch;
+
+    // Input alphabet from user 
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+
+    //  Switch ch value 
+    switch(ch)
+    {
+        case 'a': 
+        case 'e': 
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U': 
+            printf("Vowel");
+            break;
+
+        default: printf("Consonant");
+    }
+
+    return 0;
+}
