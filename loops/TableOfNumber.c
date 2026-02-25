1a42037972412a69406425d5e89aca97c88ea916
Raman verma <raman02verma@gmail.com>
2026-02-25 14:26:04 +0530

    Add table of a no program

---
+++ loops/TableOfNumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,i;
+    printf("Enter a number you want table of :");
+    scanf("%d",&x);
+
+    for(i=1; i<=10; i++)
+    {
+        printf("%d X %d=%d\n",x,i,x*i);
+    }
+    return 0;
+}
