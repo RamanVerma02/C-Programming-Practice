0613bfc44f1586a465b0ebd86cda3b0e78adc1bc
Raman verma <raman02verma@gmail.com>
2026-02-25 14:12:22 +0530

    Add find reminder program

---
+++ Basics/FindReminder.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("The Program is to find the reminder\n");
+
+    printf("The first number:\n");
+    scanf("%d",&a);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+    printf("The second number:\n");
+    scanf("%d",&b);
+    printf("The answer is:%d\n",a%b);
+    return 0;
+}
