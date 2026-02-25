63a7c00cf0b2ff8aee394584cdb03f3639f56ce2
Raman verma <raman02verma@gmail.com>
2026-02-25 14:07:55 +0530

    Add operators of c

---
+++ Basics/operator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int a,b;
+  printf("the first number is:");
+   scanf("%d", &a);
+
+   printf("The second number is:");
+   scanf("%d", &b);
+
+   printf("The sum is %d\n", a+b);
+   printf("The multiply is %d\n", a*b);
+   printf("The divide is %d\n", a/b);
+   printf("The subtract is %d\n", a-b);
+   return 0;
+}
