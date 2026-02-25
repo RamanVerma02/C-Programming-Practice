818bdfa7057b1b02c364e2d6464c3eb2c7866711
Raman verma <raman02verma@gmail.com>
2026-02-25 14:23:02 +0530

    Add for loop program

---
+++ loops/forLoops.c
@@ -0,0 +1,30 @@
+//  For loop programs , Date : 3/11/2023 , pr no 1
+
+# include <stdio.h> 
+int main()
+{
+    int num=10;
+    int i;
+
+    for ( i = 0; i < num; i++)  // for ( i=0 ; i<num ; i+=1)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+    
+}
+
+/*
+// pr no 2
+
+# include <stdio.h>
+int main ()
+{
+   for (float x=2.5; x<=20 ;x+=2.5)
+   {
+    printf("%.2f\n",x);
+   }
+    return 0;
+}
+
+*/
