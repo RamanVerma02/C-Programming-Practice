60c702acb28a8929db00d5a1d4695bcf86d56c73
Raman verma <raman02verma@gmail.com>
2026-02-25 14:09:57 +0530

    Add conditional of c

---
+++ Basics/Conditional.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=5;
+    
+    printf("%d\n",a<6 && a<10);
+    printf("%d\n",a<6 || a>10);
+
+    printf("%d\n", !a);
+    return 0;
+   
+    /*   int x=6;
+       float y=9.3;
+       int z=90;
+       int w=100;
+       x+= 4;
+       y-=3;
+       z*=10;
+       w/=50;
+
+       printf("%d\n",x);
+       printf("%f\n",y);
+       printf("%d\n%d",z,w);
+    */   
+       return 0;
+
+}
